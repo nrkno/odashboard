66852ee8a654020edb62cc9a98bdd879d48be3c0
Øyvind Holmstad <oyvind.holmstad@nrk.no>
2018-08-13 11:39:38 +0200

    feat(buildwidget): add support for small build widget

--- src/plugins/widgets/build/widget.js
+++ src/plugins/widgets/build/widget.js
@@ -8,23 +8,67 @@ var BuildWidget = function(config) {
     datasourceId: config.datasourceId,
     name: config.name,
     id: config.id,
+    displayStyle: getDisplayStyle(config),
     url: config.url,
-    width: config.width,
+    width: getWidth(config),
     height: config.height,
     started: '',
     duration: '0 s',
     percent: '',
-    status: 'Pending',
+    status: '...',
     'class': 'inProgress' 
   };
+
+  function getDisplayStyle(config) {
+    if (config.options.displayStyle !== undefined) {
+      return config.options.displayStyle;
+    } else {
+      return 'standard';
+    }
+  }
+
+  function getWidth(config) {
+    if (config.options.displayStyle === 'small') {
+      return '10%';
+    } else {
+      return config.width;
+    }
+  }
 
   widget.update = function(data) {
     _.each(data.builds, function (build) {
       if (build.buildTypeId == widget.id) {
-        updateBuildWidget(build);
+        if (widget.displayStyle === 'small') {
+          updateSmallWidget(build);
+        } else {
+          updateBuildWidget(build);
+        }
       }
     });
   };
+
+  function updateSmallWidget(build) {
+    if (build.state == 'running') {
+      widget.status = '';
+      widget.class = 'inProgress';
+      widget.percent = '(' + build.percent + '%)';
+    } else {
+      widget.percent = '';
+      if (build.state == 'failed') {
+        widget.status = ':(';
+        widget.class = 'failed';        
+      } else if (build.status == 'canceled') {
+        widget.status = ':|';
+        widget.class = 'failed';
+      } else {
+        widget.status = ':)';
+        widget.class = 'success';         
+      }
+    }
+
+    widget.started = getDateString(build.startDate, true);
+    widget.duration = getDurationString(build.duration, true);
+  }
 
   function updateBuildWidget(build) {
     if (build.state == 'running') {
@@ -49,24 +93,30 @@ var BuildWidget = function(config) {
     widget.duration = getDurationString(build.duration);
   }
 
-  function getDurationString(durationString) {
+  function getDurationString(durationString, short) {
     var duration = moment.duration(durationString);
     var hours = duration.hours();
     var minutes = duration.minutes();
     var seconds = duration.seconds();
     
-    if (hours > 0) {
+    if (hours > 0 && short) {
+      return hours + 'h ' + minutes + 'm ' + seconds + 's';
+    } else if (short) {
+      return minutes + 'm ' + seconds + 's';
+    } else if (hours > 0) {
       return hours + 'h ' + minutes + ' min ' + seconds + ' s';
     } else {
       return minutes + ' min ' + seconds + ' s';
     }
   }
 
-  function getDateString(timestamp) {
+  function getDateString(timestamp, short) {
     var date = moment(timestamp, 'YYYYMMDDTHHmmss');
-    if (date)
-      return date.format('DD-MMM HH:mm:s');
-    else
+    if (date && short)
+      return date.format('HH:mm');
+    else if (date)
+      return date.format('DD-MMM HH:mm');
+    else 
       return '.';
   }
 
